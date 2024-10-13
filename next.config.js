990f61a2bd3db822044339b132e2db440b4c622a
Mihail Kirov <me@mikirov.xyz>
2024-10-13 23:52:34 +0300

    WIP

--- next.config.js
+++ next.config.js
@@ -5,17 +5,25 @@ console.log("NODE_ENV is:", process.env.NODE_ENV);
 const isProduction = process.env.NODE_ENV === "production";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: isProduction && "export" || "standalone", // Use `export` in production and `public` in development
+let nextConfig = {
+  // output: isProduction && "export" || "standalone", // Use `export` in production and `public` in development
   // basePath: '/', // Optional, but required if you're deploying to a non-root path.
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   // Optionally, add any other Next.js config below
   reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
+  // images: {
+  //   unoptimized: true,
+  // },
 };
-
+ if (isProduction) {
+  nextConfig = {
+    ...nextConfig,
+    output: "export",
+    images: {
+      unoptimized: true,
+    }, 
+  }
+ }
 
 module.exports = withMDX(nextConfig);
